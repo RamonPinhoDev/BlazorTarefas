597776f5a6a10506c6d576095d29bf970b7230f3
RamonPinhoDev <ramon-pinho@outlook.com>
2025-11-08 23:58:20 -0300

    Finalização

---
+++ BlazorTarefas.Shared/Data/Tarefa.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorTarefas.Shared.Data
+{
+    public class Tarefa
+    {
+        public Guid id { get; set; } = Guid.NewGuid();
+
+        public string? Descricao { get; set; }
+        public bool Concluida { get; set; } 
+        public DateTime DataCriacao { get; set; }
+    }
+}
---
+++ BlazorTarefas.Shared/Entity/TarefaDados.cs
@@ -0,0 +1,55 @@
+﻿namespace BlazorTarefas.Shared.Data;
+
+    public static class TarefaDados
+    {
+
+        public static List<Tarefa> ObterTarefas() => new()
+        {
+
+         new Tarefa
+            {
+                 id = Guid.NewGuid(),
+                     Descricao = "Estudar Blazor",
+                     Concluida = false,
+                     DataCriacao = DateTime.Now
+            },
+
+                 new Tarefa
+                 {
+                     id = Guid.NewGuid(),
+                     Descricao = "Estudar Blazor",
+                     Concluida = false,
+                     DataCriacao = DateTime.Now
+                 },
+                 new Tarefa
+                 {
+                     id = Guid.NewGuid(),
+                     Descricao = "Estudar Blazor",
+                     Concluida = true,
+                     DataCriacao = DateTime.Now
+                 },
+                 new Tarefa
+                 {
+                     id = Guid.NewGuid(),
+                     Descricao = "Estudar Blazor",
+                     Concluida = false,
+                     DataCriacao = DateTime.Now
+                 },
+                 new Tarefa
+                 {  id = Guid.NewGuid(),
+                     Descricao = "Estudar Blazor",
+                     Concluida = false,
+                     DataCriacao = DateTime.Now
+                 },
+                 new Tarefa
+                 {
+                      id = Guid.NewGuid(),
+                     Descricao = "Estudar Blazor",
+                     Concluida = true,
+                     DataCriacao = DateTime.Now
+                 } ,
+
+
+        };
+    }
+
